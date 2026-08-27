70efffd9e9c945e42fab80ba8c9a20bcb41d500a
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-08-28 01:17:34 +0900

    feat: Badge 컴포넌트 구현 - 게시글 인기순 조회시 조회수 뱃지

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,14 +1,12 @@
 import styles from "./layout.module.css";
 import Header from "./_components/header/Header";
 import Navigation from "./_components/navigation/Navigation";
-import AppRail from "../(admin)/_components/appRail/AppRail";
 
 export default function PublicLayout({ children }: LayoutProps<"/">) {
   return (
     <div className={styles.container}>
       <Header />
       <Navigation />
-      <AppRail />
       {children}
     </div>
   );
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,3 @@
 export default function Blog() {
-  return <>blog 페이지입니다.</>;
+  return <>Blog 페이지 입니다.</>;
 }
